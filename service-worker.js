18722fcb37f8f5c66e96e39ad62ff78f8fa0645f
mochi-jp <the_count_of_chocolate@yahoo.co.jp>
2025-05-10 07:56:03 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,18 @@
-// service-worker.js
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open("mochi-calculator").then(function(cache) {
-      return cache.addAll([
-        "/pop-up/",
-        "/pop-up/index.html",
-        "/pop-up/manifest.json",
-        "/pop-up/service-worker.js",
-        "/pop-up/icon-192.png",
-        "/pop-up/icon-512.png"
-        // 他にCSSやJSファイルがあるならここに追加
-      ]);
-    })
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/index.html',
+                '/manifest.json'
+            ]);
+        })
+    );
 });
 
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
